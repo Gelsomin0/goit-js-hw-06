6ae24685a26b5dbca099d2d3a7ff12a55434eb37
Ivan <120274876+Gelsomin0@users.noreply.github.com>
2023-04-25 20:38:39 +0300

    9 task done

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const colorTextEl = document.querySelector('.color');
+
+buttonEl.addEventListener('click', () => {
+  const randomColor = getRandomHexColor();
+
+  colorTextEl.textContent = randomColor;
+  document.body.setAttribute('style', `background-color: ${randomColor}`);
+});
