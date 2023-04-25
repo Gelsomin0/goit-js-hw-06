46495840055e2ba89e17488a69b179c921f9c9f7
Ivan <120274876+Gelsomin0@users.noreply.github.com>
2023-04-25 20:30:08 +0300

    8 task done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const formEl = document.querySelector('.login-form');
 
+const userData = {
+    email: undefined,
+    password: undefined,
+};
+
+formEl.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    if (e.currentTarget.email.value === '' && e.currentTarget.password.value === '') {
+        alert('Please enter your email address and password!');
+    } else if (e.currentTarget.password.value === '') {
+        alert('Please enter your password!');
+    } else if (e.currentTarget.email.value === '')  {
+        alert('Please enter your email address!');
+    } else {
+        userData.email = e.currentTarget.email.value;
+        userData.password = e.currentTarget.password.value;
+        formEl.reset();
+        console.log(userData);
+    }    
+});
